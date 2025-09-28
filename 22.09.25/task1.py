ddb1205feedb32b6caa5997b3f8b58acbb80eb3c
korpusenk <korpusenkicloud.com@gmail.com>
2025-09-28 15:28:39 +0300

    summary

---
+++ 22.09.25/task1.py
@@ -0,0 +1,12 @@
+n = int(input("Введите число: "))
+for i in range(1, n + 1):
+        print(i)
+
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+if a > b:
+    print("Большее число:", a)
+elif b > a:
+    print("Большее число:", b)
