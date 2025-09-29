c6dacec3702aec4de5e20d4f162fb0e84698519e
korpusenk <korpusenkicloud.com@gmail.com>
2025-09-29 12:12:47 +0300

    commit

--- 29.09.25/task1.py
+++ 29.09.25/task1.py
@@ -1,15 +1,14 @@
 def greet(name):
     print(f"привет, {name}!")
-
 u_name = input("введите своё имя: ")
 greet(u_name)
 
 
 def square(n):
     return n**2
-
 n = int(input("введите число для возведения в квадрат: "))
 print("результат:", square(n))
+
 
 def max_of_two(x, y):
     if x>y:
@@ -19,10 +18,8 @@ def max_of_two(x, y):
     else:
         print("числа равны")
         return None
-
 a=int(input("введите первое число: "))
 b=int(input("введите второе число: "))
-
 r=max_of_two(a, b)
 if r is not None:
     print("наибольшее число:", r)
